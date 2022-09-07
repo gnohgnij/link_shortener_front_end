b9b54e5ca860a74220fbd935bbbcddbf05fac2f7
Chong Jing Hong <congjinghong@gmail.com>
2022-09-07 20:59:31 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+
+import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value);
+  };
+
+  const [newUrl, setNewUrl] = useState("");
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      let res = await fetch("https://shawwty.herokuapp.com/api/url/shorten", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          originalURL: url,
+        }),
+      });
+
+      let json = await res.json();
+      console.log(json);
+      setNewUrl(json.url.newURL);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Link Shortener</h1>
+      <form onSubmit={handleFormSubmit}>
+        <TextField
+          id="outlined-basic"
+          label="Enter your URL here"
+          variant="outlined"
+          fullWidth
+          value={url}
+          onChange={handleUrlChange}
+        />
+        <div className="center mt-1rem">
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </div>
+      </form>
+      <h1>Shortened URL:</h1>
+      <div className="center">
+        <a href={newUrl} target="_blank">
+          {newUrl}
         </a>
-      </header>
+      </div>
     </div>
   );
 }
