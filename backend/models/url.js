e469ff2037b892e6b472a1708ddfd024d63a7498
Chong Jing Hong <congjinghong@gmail.com>
2022-09-08 20:26:27 +0800

    Revert commit

--- backend/models/url.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const URLSchema = new mongoose.Schema({
-  urlCode: String,
-  originalURL: String,
-  newURL: String,
-});
-
-module.exports = mongoose.model("Url", URLSchema);
